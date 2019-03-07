5ace322ffe946bdbf9f9db8811e4083f4c4b880f
pakotoes3@gmail.com <paco@Paco>
2019-03-07 17:18:42 +0100

    He realizado el ejercicio 3

--- ejerciciosxml.py
+++ ejerciciosxml.py
@@ -10,14 +10,17 @@ def Contarnoticias(noticias):
 	numeronoticias=noticias.xpath('count(//news:news)', namespaces={'news':'https://www.google.com/schemas/sitemap-news/0.9'})
 	return int(numeronoticias)
 
+#3.Pedir una palabra o palabras clave por teclado y mostrar las noticias relacionadas con esa palabra
+
+def Titulorelacionado(cad,noticias):
+	lista=noticias.xpath('//news:news[contains(.,"%s")]/news:title/text()'%cad, namespaces={'news':'https://www.google.com/schemas/sitemap-news/0.9'})
+	return lista
 
 
 
 
-
-
-#from lxml import etree
-#noticias=etree.parse("noticias.xml")
+from lxml import etree
+noticias=etree.parse("noticias.xml")
 #anyo=input("Dime un año: ")
 #if anyo not in ('2014','2015','2016','2017','2018','2019'):
 	#print("No tengo noticias de ese año")
@@ -25,3 +28,6 @@ def Contarnoticias(noticias):
 	#for elem in Listanoticias(anyo,noticias):
 	#	print(elem)
 #print(Contarnoticias(noticias))
+#cad=input("Dime una palabra clave: ")
+#for elem in Titulorelacionado(cad,noticias):
+#	print(elem)
